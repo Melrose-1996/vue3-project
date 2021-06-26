b18a7d69e0441ba59d9762ebcb7b11d950923d63
Melrose <2713605684@qq.com>
2021-06-27 02:15:52 +0800

    feat:批量注册组件

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,18 +1,16 @@
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
-
 import defaultImg from '@/assets/images/200.png'
+
+const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install(app) {
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    importFn.keys().forEach(path => {
+      // 这样的导入是 common.js 的模块化导入， .default 是默认导出的内容
+      // 导入组件
+      const component = importFn(path).default
+      // 进行组件注册
+      app.component(component.name, component)
+    })
     defineDirective(app)
   }
 }
