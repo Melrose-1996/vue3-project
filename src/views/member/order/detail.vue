f7b8a85fc5ce8c5c5240f96c6ce193f25fa4488a
Melrose <2713605684@qq.com>
2021-08-13 01:42:34 +0800

    feat:头部展示

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,17 +1,35 @@
 <template>
-  <div class="member-detail">
-    MemberDetail
+  <div class="member-detail" v-if="order">
+    <!-- 头部 -->
+    <detail-action :order="order" />
+    <!-- 进度 -->
+    <!-- 物流 -->
+    <!-- 信息 -->
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import detailAction from './components/detail-action.vue'
+import { findOrderDetail } from '@/api/order'
+import { useRoute } from 'vue-router'
 export default {
+  components: { detailAction },
   name: 'MemberDetail',
-  setup() {}
+  setup() {
+    const route = useRoute()
+    const order = ref(null)
+    findOrderDetail(route.params.id).then(data => {
+      order.value = data.result
+    })
+    return { order }
+  }
 }
 </script>
 
 <style lang="less" scoped>
 .member-detail {
+  background-color: #fff;
+  height: 100%;
 }
 </style>
